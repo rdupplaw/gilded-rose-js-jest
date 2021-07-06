dea71a06cfc59cd57032678ea5bc5c939b1e49a0
Robert Dupplaw <dupplawrobert@gmail.com>
2021-07-06 21:09:36 +0100

    Add tests for sulfuras

--- src/shop.test.js
+++ src/shop.test.js
@@ -2,49 +2,70 @@ const Shop = require('./shop');
 
 describe('Shop', () => {
   describe('.prototype.updateQuality', () => {
+    let itemDouble;
+    let shop;
+    let items;
+
+    beforeEach(() => {
+      itemDouble = { quality: 10, sellIn: 20 };
+      shop = new Shop([itemDouble]);
+      items = shop.updateQuality();
+    });
+
     it('decreases item quality by 1', () => {
-      const itemDouble = { quality: 10 };
-      const shop = new Shop([itemDouble]);
-      const items = shop.updateQuality();
       expect(items[0].quality).toBe(9);
     });
 
     it('decreases sell in by 1', () => {
-      const itemDouble = { sellIn: 20 };
-      const shop = new Shop([itemDouble]);
-      const items = shop.updateQuality();
       expect(items[0].sellIn).toBe(19);
     });
 
     it('quality can\'t be decreased below 0', () => {
-      const itemDouble = { quality: 0 };
-      const shop = new Shop([itemDouble]);
-      const items = shop.updateQuality();
+      for (let i = 0; i < 10; i += 1) {
+        shop.updateQuality();
+      }
+      items = shop.updateQuality();
       expect(items[0].quality).toBe(0);
     });
 
     describe('when past sell by date', () => {
       it('decreases quality by 2', () => {
-        const itemDouble = { quality: 10, sellIn: 0 };
-        const shop = new Shop([itemDouble]);
-        const items = shop.updateQuality();
+        itemDouble = { quality: 10, sellIn: 0 };
+        shop = new Shop([itemDouble]);
+        items = shop.updateQuality();
         expect(items[0].quality).toBe(8);
       });
     });
 
     describe('when item is "Aged Brie"', () => {
       it('increases quality by 1', () => {
-        const itemDouble = { name: 'Aged Brie', quality: 5 };
-        const shop = new Shop([itemDouble]);
-        const items = shop.updateQuality();
+        itemDouble = { name: 'Aged Brie', quality: 5 };
+        shop = new Shop([itemDouble]);
+        items = shop.updateQuality();
         expect(items[0].quality).toBe(6);
       });
 
       it('quality can\'t exceed 50', () => {
-        const itemDouble = { name: 'Aged Brie', quality: 50 };
-        const shop = new Shop([itemDouble]);
-        const items = shop.updateQuality();
+        itemDouble = { name: 'Aged Brie', quality: 50 };
+        shop = new Shop([itemDouble]);
+        items = shop.updateQuality();
         expect(items[0].quality).toBe(50);
+      });
+    });
+
+    describe('when item is "Sulfuras, Hand of Ragnaros"', () => {
+      beforeEach(() => {
+        itemDouble = { name: 'Sulfuras, Hand of Ragnaros', sellIn: 20, quality: 80 };
+        shop = new Shop([itemDouble]);
+        items = shop.updateQuality();
+      });
+
+      it('does not change quality', () => {
+        expect(items[0].quality).toBe(80);
+      });
+
+      it('does not change sell in', () => {
+        expect(items[0].sellIn).toBe(20);
       });
     });
   });
