897e75abeb883e9ef217738b969cfb69f49df182
Robert Dupplaw <dupplawrobert@gmail.com>
2021-07-06 20:47:40 +0100

    Add test quality can't go negative

--- src/shop.test.js
+++ src/shop.test.js
@@ -16,6 +16,13 @@ describe('Shop', () => {
       expect(items[0].sellIn).toBe(19);
     });
 
+    it('quality can\'t be decreased below 0', () => {
+      const itemDouble = { quality: 0 };
+      const shop = new Shop([itemDouble]);
+      const items = shop.updateQuality();
+      expect(items[0].quality).toBe(0);
+    });
+
     describe('when passed sell by date', () => {
       it('decreases quality by 2', () => {
         const itemDouble = { quality: 10, sellIn: 0 };
