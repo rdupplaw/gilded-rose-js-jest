a3ee6faa9c1d3f9686c7d7583a49253fa8555c31
Robert Dupplaw <dupplawrobert@gmail.com>
2021-07-06 20:13:33 +0100

    Add unit tests for Item

---
+++ src/item.test.js
@@ -0,0 +1,21 @@
+const Item = require('./item');
+
+describe('Item', () => {
+  let item;
+
+  beforeEach(() => {
+    item = new Item('bar', 10, 20);
+  });
+
+  it('has a name property', () => {
+    expect(item.name).toBe('bar');
+  });
+
+  it('has a sellIn property', () => {
+    expect(item.sellIn).toBe(10);
+  });
+
+  it('has a quality property', () => {
+    expect(item.quality).toBe(20);
+  });
+});
