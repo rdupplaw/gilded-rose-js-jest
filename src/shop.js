6cfa345a3afecc08a5f01e58b1d087593fe4888f
Robert Dupplaw <dupplawrobert@gmail.com>
2021-07-06 19:53:15 +0100

    Split classes into separate files

--- src/shop.js
+++ src/shop.js
@@ -1,15 +1,8 @@
-class Item {
-  constructor(name, sellIn, quality){
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
-}
-
-class Shop {
-  constructor(items=[]){
+module.exports = class Shop {
+  constructor(items = []) {
     this.items = items;
   }
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -18,19 +11,17 @@ class Shop {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
+      } else if (this.items[i].quality < 50) {
+        this.items[i].quality = this.items[i].quality + 1;
+        if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.items[i].sellIn < 11) {
+            if (this.items[i].quality < 50) {
+              this.items[i].quality = this.items[i].quality + 1;
             }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
+          }
+          if (this.items[i].sellIn < 6) {
+            if (this.items[i].quality < 50) {
+              this.items[i].quality = this.items[i].quality + 1;
             }
           }
         }
@@ -49,19 +40,12 @@ class Shop {
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
+        } else if (this.items[i].quality < 50) {
+          this.items[i].quality = this.items[i].quality + 1;
         }
       }
     }
 
     return this.items;
   }
-}
-
-module.exports = {
-  Item,
-  Shop
-}
+};
